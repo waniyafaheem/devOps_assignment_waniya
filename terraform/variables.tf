e9bd0748ae3efbfafa63bd84542c3071cf27de61
Waniya Faheem <waniyafaheem12@gmail.com>
2025-06-15 00:20:44 +0500

    first commit

---
+++ terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_region"      { default = "us-east-1" }
+variable "instance_type"   { default = "t2.micro" }
+variable "key_name"        { default = "swarm-key" }
+variable "public_key_path" { default = "swarm_key.pub"}
+variable "ami_id"          { default = "ami-020cba7c55df1f615" }
